f5b3eca6ad7989c983be0900c83f113b64a15e27
xueli0210 <118102239+xueli0210@users.noreply.github.com>
2024-03-23 12:58:30 -0700

    Exercise 3: Guess slots

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
+import { range } from "../../utils";
 
-function Guess() {
-  return <div></div>;
+function Guess({ value }) {
+  return (
+    <p className="guess">
+      {range(5).map((index) => (
+        <span className="cell" key={index}>
+          {value ? value[index] : ""}
+        </span>
+      ))}
+    </p>
+  );
 }
 
 export default Guess;
