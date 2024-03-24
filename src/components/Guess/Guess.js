26988824d986fdd81b8eddfce46e2731aee9732a
xueli0210 <118102239+xueli0210@users.noreply.github.com>
2024-03-23 19:50:35 -0700

    Exercise 4: Game logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { range } from "../../utils";
+import { checkGuess } from "../../game-helpers.js";
 
-function Guess({ value }) {
+function Guess({ value, answer }) {
+  const letterCheckedArray = checkGuess(value, answer);
   return (
     <p className="guess">
       {range(5).map((index) => (
-        <span className="cell" key={index}>
+        <span
+          className={
+            value ? `cell ${letterCheckedArray[index].status}` : "cell"
+          }
+          key={index}
+        >
           {value ? value[index] : ""}
         </span>
       ))}
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,11 +3,11 @@ import { range } from "../../utils";
 import { NUM_OF_GUESSES_ALLOWED } from "../../constants";
 import Guess from "../Guess/Guess";
 
-function GuessResults({ guesses }) {
+function GuessResults({ answer, guesses }) {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((index) => (
-        <Guess value={guesses[index]} key={index} />
+        <Guess value={guesses[index]} key={index} answer={answer} />
       ))}
     </div>
   );
